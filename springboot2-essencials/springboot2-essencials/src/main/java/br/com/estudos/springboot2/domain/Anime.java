cc332cc236a626c6923b8882b45d9643c6834c30
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-08 22:31:03 -0300

    Create Anime.java

---
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/domain/Anime.java
@@ -0,0 +1,14 @@
+package br.com.estudos.springboot2.domain;
+
+public class Anime {
+	private String name;
+
+	public Anime(String name) {
+		this.name = name;
+	}
+
+	public Anime() {
+		
+	}
+	
+}
