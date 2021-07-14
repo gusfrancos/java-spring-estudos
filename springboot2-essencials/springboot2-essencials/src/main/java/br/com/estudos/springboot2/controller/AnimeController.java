f5284978974396a0f1c1481ff6684cfe843c6c2d
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-14 08:08:30 -0300

    Update AnimeController.java

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/controller/AnimeController.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package br.com.estudos.springboot2.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudos.springboot2.domain.Anime;
+import br.com.estudos.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+
+//terceira forma de fazer é com @RequiredArgsConstructor (Ele cria um construtor com todos os campos)
+@RequiredArgsConstructor
 public class AnimeController {
+	
+	//Primeira forma de fazer é com autowired sem constructor
+	//@Autowired
+	//private DateUtil dateUtil;
+	
+	//Segunda forma de fazer é sem autowired e com constructor
+//	private DateUtil dateUtil;
+//	public AnimeController(DateUtil dateUtil) {
+//		this.dateUtil = dateUtil;
+//	}
+
+	
+	private DateUtil dateUtil;
+	
 	@GetMapping(path = "list")
 	public List<Anime> list(){
+		log.info(dateUtil.formatLocalDateTimeYoDataBaseStyle(LocalDateTime.now()));
 		return List.of(new Anime("DBZ"), new Anime("Berserk"));
 	}
 }
