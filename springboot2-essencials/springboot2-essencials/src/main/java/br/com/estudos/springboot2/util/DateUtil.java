2d1927e94dd61606db6290ba607fc55ddb86db75
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-28 13:35:15 -0300

    Update DateUtil.java

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/util/DateUtil.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/util/DateUtil.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Component;
 
+//@component transforma a classe em um bean
+//@Service e repository tem objetivos semelhantes que o @Component
 @Component
 public class DateUtil {
 	public String formatLocalDateTimeYoDataBaseStyle(LocalDateTime localDateTime) {
@@ -12,3 +14,20 @@ public class DateUtil {
 		
 	}
 }
+
+/**@Component
+Anotação genérica para qualquer componente gerenciado pelo Spring. 
+Esta anotação faz com que o bean registrado no Spring possa ser utilizado em qualquer bean, seja ele um serviço, um DAO, um controller, etc. 
+No nosso exemplo, ele será responsável por um Bean que representa uma entidade.
+*/
+
+/**A anotação @Service representa esta classe como se fosse um serviço. 
+ * Já as anotações @Autowired permitem que o Spring injete as dependências nesta classe
+ */
+
+/**
+ * Repository e Configuração do Spring. Repository (em português, repositório) 
+ * é um padrão de projeto descrito no livro Domain-Driven Design (DDD) de Eric Vans. 
+ * É um conceito muito semelhante ao padrão de projeto DAO, já que seu foco também é
+ *  a camada de persistência de dados de uma aplicação.
+ */  
