3fc4cdd8bf5809322c0b337c03c323c26a50861c
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-02 14:37:40 -0300

    Update AnimeController.java

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/controller/AnimeController.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/controller/AnimeController.java
@@ -12,16 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudos.springboot2.domain.Anime;
 import br.com.estudos.springboot2.service.AnimeService;
 import br.com.estudos.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
 
 @RestController
 @RequestMapping("animes")
@@ -30,86 +27,76 @@ import lombok.extern.log4j.Log4j2;
 //terceira forma de fazer é com @RequiredArgsConstructor (Ele cria um construtor com todos os campos que são finais)
 @RequiredArgsConstructor
 public class AnimeController {
-	
-	//Primeira forma de fazer é com autowired sem constructor
-	//@Autowired
-	//private DateUtil dateUtil;
-	
-	//Segunda forma de fazer é sem autowired e com constructor
+
+	// Primeira forma de fazer é com autowired sem constructor
+	// @Autowired
+	// private DateUtil dateUtil;
+
+	// Segunda forma de fazer é sem autowired e com constructor
 //	private DateUtil dateUtil;
 //	public AnimeController(DateUtil dateUtil) {
 //		this.dateUtil = dateUtil;
 //	}
 
-	
 	private final DateUtil dateUtil;
 	private final AnimeService animeService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Anime>> list(){
+	public ResponseEntity<List<Anime>> list() {
 		log.info(dateUtil.formatLocalDateTimeYoDataBaseStyle(LocalDateTime.now()));
-		//return animeService.listAll();
-		
-		//boa pratica: retornar o status desta requição
-		//return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
-		
-		
-		//outra forma
+		// return animeService.listAll();
+
+		// boa pratica: retornar o status desta requição
+		// return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+
+		// outra forma
 		return ResponseEntity.ok(animeService.listAll());
 	}
-	
-	
-	
+
 	@GetMapping(path = "/{id}")
-	public ResponseEntity<Anime> findById(@PathVariable long id){
+	public ResponseEntity<Anime> findById(@PathVariable long id) {
 		log.info(dateUtil.formatLocalDateTimeYoDataBaseStyle(LocalDateTime.now()));
-		
-		
-		//boa pratica: retornar o status desta requição
+
+		// boa pratica: retornar o status desta requição
 		return new ResponseEntity<>(animeService.findById(id), HttpStatus.OK);
-		
-		
-		//outra forma
-		//return ResponseEntity.ok(animeService.listAll().get(id));
+
+		// outra forma
+		// return ResponseEntity.ok(animeService.listAll().get(id));
 	}
-	
+
 //	@GetMapping(path = "list2")
 //	public List<Anime> list2(){
 //		log.info(dateUtil.formatLocalDateTimeYoDataBaseStyle(LocalDateTime.now()));
 //		return List.of(new Anime("One Piece"), new Anime("Berserk"), new Anime("hdhfhf"));
 //	}
-	
+
 	@PostMapping
-    public ResponseEntity<Anime> save(@RequestBody Anime anime){
-        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
-    }
-	
+	public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+		return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+	}
+
 	@DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable long id) {
-        animeService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-	
+	public ResponseEntity<Void> delete(@PathVariable long id) {
+		animeService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
 	//
 	@PutMapping
-    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
-        animeService.replace(anime);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-	
-	
+	public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+		animeService.replace(anime);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
 }
 
-
 /**
- * => A anotação @RestController permite definir um controller com características REST;
- * => A anotação @Autowired delega ao Spring Boot a inicialização do objeto;
- * => A anotação @RequestMapping permite definir uma rota. Caso não seja informado o método HTTP da rota, ela será definida para todos os métodos.
- * => A anotação @PathVariable indica que o valor da variável virá de uma informação da rota;
- * => A anotação @RequestBody indica que o valor do objeto virá do corpo da requisição;
- * => E a anotação @Valid indica que os dados recebidos devem ser validados.
+ * => A anotação @RestController permite definir um controller com
+ * características REST; => A anotação @Autowired delega ao Spring Boot a
+ * inicialização do objeto; => A anotação @RequestMapping permite definir uma
+ * rota. Caso não seja informado o método HTTP da rota, ela será definida para
+ * todos os métodos. => A anotação @PathVariable indica que o valor da variável
+ * virá de uma informação da rota; => A anotação @RequestBody indica que o valor
+ * do objeto virá do corpo da requisição; => E a anotação @Valid indica que os
+ * dados recebidos devem ser validados.
  */
-
-
-
-
