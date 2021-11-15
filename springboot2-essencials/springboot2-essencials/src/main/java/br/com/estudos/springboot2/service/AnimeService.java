24a36da90b134223e7353d1e16c8f0a373fccb83
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-15 20:45:11 -0300

    BadRequestException

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/service/AnimeService.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/estudos/springboot2/service/AnimeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.estudos.springboot2.domain.Anime;
+import br.com.estudos.springboot2.exception.BadRequestException;
 import br.com.estudos.springboot2.mapper.AnimeMapper;
 import br.com.estudos.springboot2.repository.AnimeRepository;
 import br.com.estudos.springboot2.requests.AnimePostRequestBody;
@@ -29,8 +30,13 @@ public class AnimeService {
 
     public Anime findByIdOrThrowBadRequestException(long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+                .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
+    
+//    public Anime findByIdOrThrowBadRequestException(long id) {
+//        return animeRepository.findById(id)
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+//    }
 
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
