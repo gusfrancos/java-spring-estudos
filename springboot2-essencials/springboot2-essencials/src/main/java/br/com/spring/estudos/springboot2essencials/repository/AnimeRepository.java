7433f1ef2fdf7f5bc6683e4729a6800b498549c8
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2022-05-16 07:47:56 -0300

    End JPA

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/repository/AnimeRepository.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/repository/AnimeRepository.java
@@ -1,9 +1,9 @@
 package br.com.spring.estudos.springboot2essencials.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.spring.estudos.springboot2essencials.domain.Anime;
 
-public interface AnimeRepository {
-    List<Anime> listAll();
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
 }
