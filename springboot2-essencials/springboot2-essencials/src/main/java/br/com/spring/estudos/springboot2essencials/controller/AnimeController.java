3e77d34840bddf2062e50dfefffd836e9644751d
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2022-05-19 10:19:54 -0300

    Correção do Spring JPA

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/controller/AnimeController.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/controller/AnimeController.java
@@ -27,14 +27,28 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequestMapping("animes")
 @Log4j2
+
+//terceira forma de fazer é com @RequiredArgsConstructor (Ele cria um construtor com todos os campos que são finais)
 @RequiredArgsConstructor
 public class AnimeController {
+	
+	// Primeira forma de fazer é com autowired sem constructor
+		// @Autowired
+		// private DateUtil dateUtil;
+
+		// Segunda forma de fazer é sem autowired e com constructor
+//		private DateUtil dateUtil;
+//		public AnimeController(DateUtil dateUtil) {
+//			this.dateUtil = dateUtil;
+//		}
+	
+	
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
     @GetMapping
     public ResponseEntity<List<Anime>> list() {
-
+        log.info(dateUtil.formatLocalDateTimeYoDataBaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.listAll());
     }
 
@@ -59,4 +73,15 @@ public class AnimeController {
         animeService.replace(animePutRequestBody);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+}
 
+/**
+ * => A anotação @RestController permite definir um controller com
+ * características REST; => A anotação @Autowired delega ao Spring Boot a
+ * inicialização do objeto; => A anotação @RequestMapping permite definir uma
+ * rota. Caso não seja informado o método HTTP da rota, ela será definida para
+ * todos os métodos. => A anotação @PathVariable indica que o valor da variável
+ * virá de uma informação da rota; => A anotação @RequestBody indica que o valor
+ * do objeto virá do corpo da requisição; => E a anotação @Valid indica que os
+ * dados recebidos devem ser validados.
+ */
--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/service/AnimeService.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/service/AnimeService.java
@@ -4,12 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.spring.estudos.springboot2essencials.domain.Anime;
-import br.com.spring.estudos.springboot2essencials.exception.BadRequestException;
-import br.com.spring.estudos.springboot2essencials.mapper.AnimeMapper;
 import br.com.spring.estudos.springboot2essencials.repository.AnimeRepository;
 import br.com.spring.estudos.springboot2essencials.request.AnimePostRequestBody;
 import br.com.spring.estudos.springboot2essencials.request.AnimePutRequestBody;
@@ -18,4 +15,38 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AnimeService {
+
+    private final AnimeRepository animeRepository;
+
+    public List<Anime> listAll() {
+        return animeRepository.findAll();
+    }
+
+    public Anime findByIdOrThrowBadRequestException(long id) {
+        return animeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(AnimePostRequestBody animePostRequestBody) {
+        return animeRepository.save(Anime.builder().name(animePostRequestBody.getName()).build());
+    }
+
+    public void delete(long id) {
+        animeRepository.delete(findByIdOrThrowBadRequestException(id));
+    }
+
+    public void replace(AnimePutRequestBody animePutRequestBody) {
+        Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
+        Anime anime = Anime.builder()
+                .id(savedAnime.getId())
+                .name(animePutRequestBody.getName())
+                .build();
+
+        animeRepository.save(anime);
+    }
+}
+
+/**A anotação @Service representa esta classe como se fosse um serviço. 
+ * Já as anotações @Autowired permitem que o Spring injete as dependências nesta classe
+ */
 
