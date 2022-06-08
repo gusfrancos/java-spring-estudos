b1003808d877769ffd1367689597176fbb32192f
gusfrancos <gusfrancos@gmail.com>
2022-06-08 07:35:36 -0300

    Unit tests

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/request/AnimePostRequestBody.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/request/AnimePostRequestBody.java
@@ -2,11 +2,12 @@ package br.com.spring.estudos.springboot2essencials.request;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
-    
 }
---
+++ springboot2-essencials/springboot2-essencials/src/test/java/br/com/spring/estudos/springboot2essencials/util/AnimePutRequestBodyCreator.java
@@ -0,0 +1,12 @@
+package br.com.spring.estudos.springboot2essencials.util;
+
+import br.com.spring.estudos.springboot2essencials.request.AnimePutRequestBody;
+
+public class AnimePutRequestBodyCreator {
+    public static AnimePutRequestBody createAnimePutRequestBody(){
+        return AnimePutRequestBody.builder()
+                .id(AnimeCreator.createValidUpdatedAnime().getId())
+                .name(AnimeCreator.createValidUpdatedAnime().getName())
+                .build();
+    }
+}
