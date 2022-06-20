357663732a45d8ca25fdb5cee9809fba6188ebae
gusfrancos <gusfrancos@gmail.com>
2022-06-20 19:08:40 -0300

    Security CSRF

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/config/SecurityConfig.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/config/SecurityConfig.java
@@ -7,20 +7,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @EnableWebSecurity
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+       // http.csrf().disable()
+             http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
