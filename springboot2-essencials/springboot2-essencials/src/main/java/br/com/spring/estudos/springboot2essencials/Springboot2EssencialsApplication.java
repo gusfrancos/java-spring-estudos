2b614ec31e2123a6e3eb0da905e5313873be6829
gusfrancos <gusfrancos@gmail.com>
2022-06-25 17:36:58 -0300

    Grafana

--- springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/Springboot2EssencialsApplication.java
+++ springboot2-essencials/springboot2-essencials/src/main/java/br/com/spring/estudos/springboot2essencials/Springboot2EssencialsApplication.java
@@ -1,7 +1,12 @@
 package br.com.spring.estudos.springboot2essencials;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "br.com.spring.estudos.springboot2essencials")
@@ -9,6 +14,12 @@ public class Springboot2EssencialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2EssencialsApplication.class, args);
+	}
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 
 }
