68a9b01c6be65ca0e2274d111f1ee4dadd5d850b
gusfrancos <gusfrancos@gmail.com>
2022-07-06 08:01:52 -0300

    Configurações iniciais do projeto de estudo de microservicos com spring

---
+++ springboot-microservice/core/src/test/java/br/com/spring/estudos/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.estudos.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
